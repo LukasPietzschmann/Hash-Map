d9e73d24626f01c089bd7b42a95b2bbf37798eb2
Remoo-t <joseph.rieger@yahoo.de>
2019-11-09 11:55:07 +0100

    implemented linProb

--- src/LinearProbing.java
+++ src/LinearProbing.java
@@ -1,7 +1,22 @@
 // Sondierungssequenz gemäß linearer Sondierung.
 class LinearProbing extends AbstractHashSequence {
   // Lineare Sondierung mit Streuwertfunktion f.
+
+  private static int prev;
+
+
   public LinearProbing (HashFunction f) {
     super(f);
   }
+
+  @Override
+  public int first(Object key) {
+    return prev = func.compute(key);
+  }
+
+  @Override
+  public int next() {
+    return prev = (prev + 1) % size();
+  }
 }
+
