45f57c83d8f031ad0eefd2f266bb0298115a2af1
Luke <lukaspie00@gmail.com>
2019-11-09 12:15:24 +0100

    Double hashing

--- src/DoubleHashing.java
+++ src/DoubleHashing.java
@@ -2,10 +2,23 @@
 class DoubleHashing extends AbstractHashSequence {
   // Zweite Streuwertfunktion.
   private HashFunction func2;
+  private Object key;
+  private int it = 0;
   
   // Doppelte Streuung mit Streuwertfunktionen f1 und f2.
   public DoubleHashing (HashFunction f1, HashFunction f2) {
     super(f1);
     func2 = f2;
+  }
+  
+  @Override
+  public int first(Object key) {
+    this.key = key;
+    return (func.compute(key) % func.size());
+  }
+  
+  @Override
+  public int next() {
+    return (func.compute(key) + ++it * func2.compute(key)) % func2.size();
   }
 }
