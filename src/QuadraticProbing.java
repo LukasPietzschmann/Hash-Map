018fb5d13ada016be4806291a76c1a93188729c8
Jannik <jannik.pflieger@gmail.com>
2019-11-09 12:14:29 +0100

    DivisionMethod&QuadraticProbing

--- src/QuadraticProbing.java
+++ src/QuadraticProbing.java
@@ -1,8 +1,23 @@
 // Sondierungssequenz gemäß quadratischer Sondierung
 // (Implementierung nur mit Ganzzahlarithmetik).
 class QuadraticProbing extends AbstractHashSequence {
+
+  private static int j;
+  private int first;
   // Quadratische Sondierung mit Streuwertfunktion f.
   public QuadraticProbing (HashFunction f) {
     super(f);
+    j = 0;
+  }
+
+  @Override
+  public int first(Object key) {
+    return prev= func.compute(key);
+  }
+
+  @Override
+  public int next() {
+    j = j + 1;
+    return (prev + j) % size();
   }
 }
